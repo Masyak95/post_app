9e1c8168c15fdd4d8f37ce0d04ba34855f31f434
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-06-14 11:32:51 +0200

    post form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {useRef, useState} from "react";
+import { useState} from "react";
 import PostList from "./components/PostList/PostList";
 import MyInput from "./components/UI/input/MyInput";
 import MyButton from "./components/UI/button/MyButton";
+import PostForm from "./components/PostForm/PostForm";
 
 function App() {
 
@@ -12,40 +13,14 @@ function App() {
         {id: 3, title: "React", body: "description"}
     ])
 
-    const [title, setTitle] = useState("")
-    const [body, setBody] = useState("")
-
-
-    const addNewPost = (e) => {
-        e.preventDefault()
-        const newPost = {
-            id: Date.now(),
-            title,
-            body
-        }
-        console.log(newPost)
+    const createPost = ( newPost)=>{
         setPosts([...posts, newPost])
-        setTitle("")
-        setBody(" ")
     }
+
 
     return (
         <div className="App">
-            <form>
-                <MyInput
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                    type={"text"}
-                    placeholder={"title"}
-                />
-                <MyInput
-                    value={body}
-                    onChange={e => setBody(e.target.value)}
-                    type={"text"}
-                    placeholder={"description"}
-                />
-                <MyButton onClick={addNewPost}>Create post</MyButton>
-            </form>
+            <PostForm create={createPost}/>
             <PostList posts={posts} title={"post list number 1"}/>
         </div>
     );
