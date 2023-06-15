47a1a814cbe920bfe938314a442412069f0a77d4
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-06-15 17:24:35 +0200

    test

--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { v4 as uuid4 } from 'uuid';
 
-const MySelect = ({ options, defaultValue, value, onChange }) => {
+const MySelect = ({options, defaultValue, value, onChange}) => {
     return (
-        <select value={value} onChange={e => onChange(e.target.value)}>
+        <select
+            value={value}
+            onChange={event => onChange(event.target.value)}
+        >
             <option disabled value="">{defaultValue}</option>
-            {options.map(option => (
-                <option key={uuid4()} value={option.value}>
+            {options.map(option =>
+                <option key={option.value} value={option.value}>
                     {option.name}
                 </option>
-            ))}
+            )}
         </select>
     );
 };
