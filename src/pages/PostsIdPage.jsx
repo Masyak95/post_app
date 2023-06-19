a859984ad030a345a8fd71d181559c6bd845909f
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-06-19 17:51:12 +0200

    code refactoring

--- src/pages/PostsIdPage.jsx
+++ src/pages/PostsIdPage.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+import {useFetching} from "../hooks/useFetching";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/loader/Loader";
 
 const PostsIdPage = () => {
     const params = useParams()
-    console.log(params)
+    const [post, setPost] = useState({})
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(id)
+        setPost(response.data)
+    })
+
+    useEffect(() => {
+        fetchPostById(params.id)
+    },[])
+
     return (
         <div>
             <h1>You have accessed the post page with ID {params.id}</h1>
+            {
+                isLoading
+                    ? <Loader/>
+                    : <div>{post.id}, {post.title}</div>
+            }
         </div>
     );
 };
