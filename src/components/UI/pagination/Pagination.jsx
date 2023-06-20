2266ae5c1f552e5b68079394dd0877dfddf4a531
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-06-20 16:46:37 +0200

    code refactoring

--- src/components/UI/pagination/Pagination.jsx
+++ src/components/UI/pagination/Pagination.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import usePagination from "../../../hooks/usePagination";
-
+import s from"./Pagination.module.css"
 const Pagination = ({totalPages, page, changePage}) => {
 
     const pagesArray = usePagination(totalPages);
 
     return (
-        <div className={"page__wrapper"}>
+        <div className={s.wrapper}>
             {pagesArray.map((p) => (
                 <span
                     onClick={()=>changePage(p)}
-                    className={page === p ? "page page__current" : "page"}
+                    className={page === p ? `${s.page} ${s.current}` : s.page}
                     key={p}
                 >
                         {p}
